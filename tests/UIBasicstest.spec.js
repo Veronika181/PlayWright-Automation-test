cc6e890fcaa244faf6705ab6600420917f2fed9c
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-10-15 16:10:19 +0200

    update UIBasicstest.spec.js

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -2,11 +2,29 @@
 const {test} = require('@playwright/test'); //module @playwright/test
 
 //test case name, test function
-test('First Playwright test', function()
+test('First Playwright test', async ({broswer, page})=>
 {
 //playwright code-
 //step1 - open browser
 //step2 - enter u/p 2seconds
 //step3 - click 
-
+    // chrome - plugins/ cookies
+        //const context = await broswer.newContext();
+        //const page = await context.newPage();
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
+
+test('Browser Context Playwright test', async ({broswer, page})=>
+{
+        chrome - plugins/ cookies
+            const context = await broswer.newContext();
+            const page = await context.newPage();
+            await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
+ 
+test('Page Playwright test', async ({page})=>
+{
+      await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+      //
+
+    });
