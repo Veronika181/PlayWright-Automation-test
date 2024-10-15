9521a03cf02d2aefe954030ac19acf9e04f3f120
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-10-15 16:31:16 +0200

    up

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -17,7 +17,7 @@ test('First Playwright test', async ({browser, page})=>
 test('Browser Context Playwright test', async ({browser})=>
 {
     chrome - plugins/ cookies
-      const context = await broswer.newContext();
+      const context = await browser.newContext();
       const page = await context.newPage();
       await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
