011f8b3266f32e0b5ec4db851a1b229e2567160e
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-10-15 15:18:06 +0200

    update

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,11 @@
+//first import require test
+const {test} = require('@playwright/test'); //module @playwright/test
+
+//test case name, test function
+test('First Playwright test', function()
+{
+//playwright code-
+//step1
+//step2
+//step3
+});
