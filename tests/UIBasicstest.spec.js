1ee929d9b734bd6dce416a03215b77b4e9c3d8c0
Veronika <99502903+Veronika181@users.noreply.github.com>
2024-10-16 16:51:24 +0200

    update

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -15,20 +15,31 @@ test('First Playwright test', async ({browser, page})=>
 });
 
 test.only('Browser Context Playwright test', async ({browser})=>
-{
+{   
     //chrome - plugins/ cookies
       const context = await browser.newContext();
       const page = await context.newPage();
+      const userName = page.locator('#username');
+      const signIn = page.locator('#signInBtn');
+      const cardTitles = page.locator(".card-body a")
       await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
       console.log(await page.title());
-      //css, xpath
-      await page.locator('#username').fill("rahulshetty");
+      await userName.fill("rahulshetty");
       await page.locator("[type='password']").fill("learning")
-      await page.locator("#signInBtn").click();
-      //webdriverwait
-      console.log(await page.locator("[style*='block']").textContent());
-      await expect(page.locator("[style*='block']")).toContainText('Incorrect');
-})
+      await signIn.click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+    
+    console.log(await cardTitles.first().textContent());
+  console.log(await cardTitles.nth(1).textContent());
+  const allTitles = await cardTitles.allTextContents();
+  
+  console.log(allTitles);
+
+});
  
 
 test.only('Page Playwright test', async ({page})=>
